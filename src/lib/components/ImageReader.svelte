23f15eda8491513ca903d50e5637514137d01f46
Khanh Duy Nguyen <kduyng71@gmail.com>
2023-12-15 09:23:26 -0500

    change server images fetch

--- src/lib/components/ImageReader.svelte
+++ src/lib/components/ImageReader.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	console.log("hello");
+	import { base } from "$app/paths";
 	export let json = {
 		url: "https://via.placeholder.com/400x400",
+		id: "1234",
 	};
 	$: console.log("json", json);
 </script>
 
 <div class="image">
-	<img class="img" src={json.url ?? "https://via.placeholder.com/400x400"} />
+	<img class="img" src={base + "/images/" + json.id} />
 </div>
