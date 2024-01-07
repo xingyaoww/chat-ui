fe5096d498a1452764943418c44bbad2338baada
Xingyao Wang <xingyao6@illinois.edu>
2024-01-06 22:10:47 -0600

    update style for execute and stdout block

--- src/lib/components/CodeBlock.svelte
+++ src/lib/components/CodeBlock.svelte
@@ -6,33 +6,45 @@
 	export let lang = "";
 
 	$: highlightedCode = "";
+	let scrollToBottomElement: HTMLPreElement;
 
 	afterUpdate(async () => {
 		const { default: hljs } = await import("highlight.js");
 		const language = hljs.getLanguage(lang);
 
 		highlightedCode = hljs.highlightAuto(code, language?.aliases).value;
+
+		// Scroll to bottom whenever highlightedCode changes
+		if (scrollToBottomElement) {
+			scrollToBottomElement.scrollTop = scrollToBottomElement.scrollHeight;
+      }
 	});
 </script>
 
 <div class="group relative my-4 rounded-lg">
 	<!-- eslint-disable svelte/no-at-html-tags -->
 	{#if lang === "result"}
-		<div class="rounded-lg bg-gray-700 p-4 text-xs">
+		<div class="rounded-lg bg-gray-800 p-4 text-xs">
 			<div class="mb-1 text-gray-400">STDOUT/STDERR</div>
-			<div class="prose flex flex-col-reverse text-white">
-				{@html code
-				.replaceAll("<", "&lt;")
-				.replaceAll(">", "&gt;")
-				.replaceAll("\n", "<br />")
-				}
-			</div>
+			<pre
+			bind:this={scrollToBottomElement}
+			class="scrollbar-custom scrollbar-thumb-gray-500 hover:scrollbar-thumb-gray-400 dark:scrollbar-thumb-white/10 dark:hover:scrollbar-thumb-white/20 overflow-auto px-5 max-h-[60vh]"
+			style="padding-left: 0; padding-right: 0; padding-bottom: 0; padding-top: 0; font-size: 0.75rem;"
+			><code
+				class="language-console">{@html highlightedCode || code.replaceAll("<", "&lt;")}</code
+			></pre>
 		</div>
 	{:else}
-		<pre
-			class="scrollbar-custom scrollbar-thumb-gray-500 hover:scrollbar-thumb-gray-400 dark:scrollbar-thumb-white/10 dark:hover:scrollbar-thumb-white/20 overflow-auto px-5"><code
+		<div class="rounded-lg bg-gray-800 p-4 text-xs">
+			<div class="mb-1 text-gray-400">EXECUTE</div>
+			<pre
+			class="scrollbar-custom scrollbar-thumb-gray-500 hover:scrollbar-thumb-gray-400 dark:scrollbar-thumb-white/10 dark:hover:scrollbar-thumb-white/20 overflow-auto px-5"
+			style="padding-left: 0; padding-right: 0; padding-bottom: 0; padding-top: 0; font-size: 0.75rem;"
+			><code
 				class="language-{lang}">{@html highlightedCode || code.replaceAll("<", "&lt;")}</code
 			></pre>
+		</div>
+
 		<CopyToClipBoardBtn
 			classNames="absolute top-2 right-2 invisible opacity-0 group-hover:visible group-hover:opacity-100"
 			value={code}
